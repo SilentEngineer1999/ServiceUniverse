374f79dfa65a22e6177e641965cd7426fea56597
Oriel <oriel.a.tabilo@gmail.com>
2025-10-09 17:55:16 +1100

    fix bugs

--- utilityMicroservice/Program.cs
+++ utilityMicroservice/Program.cs
@@ -7,7 +7,12 @@ using System.Threading;
 var builder = WebApplication.CreateBuilder(args);
 
 // Users database
-var connectionString = builder.Configuration.GetConnectionString("Default");
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql("Host=db-utility;Port=5432;Database=usersdb;Username=postgres;Password=root"));
+
+// Utilities database
+builder.Services.AddDbContext<UtilityDbContext>(options =>
+    options.UseNpgsql("Host=db-utility;Port=5432;Database=postgres;Username=postgres;Password=root"));
 
 // HttpClient for Users microservice validation
 builder.Services.AddHttpClient();
